1c8f8efdc19e7728fed27a6119817872c0152fcd
LoesJanssen <loesjanssen89@gmail.com>
2020-01-25 13:12:03 +0100

    Fetched data API with componentDidMount

--- my_quotegarden_app/src/App.js
+++ my_quotegarden_app/src/App.js
@@ -5,23 +5,9 @@ import Quote from "./components/Quote";
 function App() {
   return (
     <div className="App">
-      <h1>
+      <h6>
         <QuoteSearcher />
-      </h1>
-      <h4>
-        <Quote
-          text="Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind"
-          author="Bruce Lee"
-        />
-        <Quote
-          text="Give me six hours to chop down a tree and I will spend the first four sharpening the axe."
-          author="Abraham Lincoln"
-        />
-        <Quote
-          text="Good timber does not grow with ease; the stronger the wind, the stronger the trees."
-          author="J. Willard Marriott"
-        />
-      </h4>
+      </h6>
     </div>
   );
 }
