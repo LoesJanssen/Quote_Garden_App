2e3f96db5b7e0f00e42826fbebe7430342116fd1
LoesJanssen <loesjanssen89@gmail.com>
2020-01-25 15:13:01 +0100

    Add Like & Dislike buttons in Quote Component

--- my_quotegarden_app/src/App.js
+++ my_quotegarden_app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import QuoteSearcher from "./components/QuoteSearcher";
-
+import Quote from "./components/Quote";
 function App() {
   return (
     <div className="App">
       <h6>
         <QuoteSearcher />
       </h6>
+      <Quote />
     </div>
   );
 }
--- my_quotegarden_app/src/components/Quote.js
+++ my_quotegarden_app/src/components/Quote.js
@@ -5,8 +5,9 @@ export default class Quote extends Component {
     return (
       <div>
         <h2>{this.props.text}</h2>
-
         <h3>By: {this.props.author}</h3>
+        <button>Like</button>
+        <button>Dislike</button>
       </div>
     );
   }
