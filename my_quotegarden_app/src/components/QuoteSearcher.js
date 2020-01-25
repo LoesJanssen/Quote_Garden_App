37a83e0d98ff4945b1b71d95a7ba362455e9a494
LoesJanssen <loesjanssen89@gmail.com>
2020-01-25 12:08:40 +0100

    Quotes from state in QuoteSearcher.js

--- my_quotegarden_app/src/components/QuoteSearcher.js
+++ my_quotegarden_app/src/components/QuoteSearcher.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Quote from "./Quote";
 
 export default class QuoteSearcher extends Component {
   state = {
@@ -25,10 +26,23 @@ export default class QuoteSearcher extends Component {
   };
 
   render() {
-    console.log();
+    // const testQuote = this.state.quotes.map(function(quote) {
+    //   return quote.quoteAuthor;
+    // });
+    // console.log(testQuote);
+
     return (
       <div>
         <h1> Quotes </h1>
+        <ul>
+          {this.state.quotes.map(quote => (
+            <Quote
+              text={quote.quoteText}
+              author={quote.quoteAuthor}
+              key={quote._id}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
